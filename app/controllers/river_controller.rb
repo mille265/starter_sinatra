717b963b25a5526d22874bcb6c732c2f923e12f9
Emily Bolten <emilybolten@gmail.com>
2019-06-05 16:42:01 -0400

    updated, its working wooohoo

--- app/controllers/river_controller.rb
+++ app/controllers/river_controller.rb
@@ -1,21 +1,21 @@
 require 'pry'
 class RiverController < Sinatra::Base
 
+    set :method_override, true
     set :views, "app/views/rivers"
 
+    #displays index page with all rivers
     get "/rivers" do
         @rivers = River.all
         erb :index
     end 
 
+    #will create a new river with /rivers/new form
     post "/rivers" do
-        #creates a new river
         continent_name = params["name_continent"]
         continent = Continent.find_or_create_by(name: continent_name)
         
         river_name = params['name_river']
-
-        
 
         num_countries_pass_through = params["num_countries_pass_through"]
         river = River.create(name: river_name, continent_id: continent.id, num_countries_pass_through: num_countries_pass_through)
@@ -23,14 +23,55 @@ class RiverController < Sinatra::Base
         redirect "rivers/#{river.id}"
     end
 
+    #get method to display form for new river
     get "/rivers/new" do
         erb :new
     end
         
+    #finding a river by the id
     get "/rivers/:id" do
         id = params[:id]
         @river = River.find_by_id(id)
         erb :show 
     end
         
+    #edit page for river
+    get "/rivers/:id/edit" do
+
+        id = params[:id]
+        @river = River.find(id)
+
+        erb :edit
+    end
+
+
+    #edit a river
+    patch "/rivers/:id" do
+        id = params[:id]
+        @river = River.find(id)
+
+        continent_name = params['name_continent']
+        id_continent = Continent.find_or_create_by(name: continent_name)
+        id_continent = id_continent.id
+
+        # binding.pry
+
+
+        @river.update(name: params[:name_river],
+            continent_id: id_continent,
+            num_countries_pass_through: params[:num_countries_pass_through])
+
+        redirect "/rivers/#{@river.id}"
+    end
+
+    #delete a river
+    delete "/rivers/:id" do
+        id = params[:id]
+        @river = River.find(id)
+        @river.destroy
+
+        redirect "/rivers"
+    end
+
+
 end 
